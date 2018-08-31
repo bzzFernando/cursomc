bf897cc2edc63314fe9fbbf0d1746c724a33853c
FrMurta <fernandormurta@gmail.com>
2018-08-31 17:33:37 -0300

    Aula 21 - Inicio

---
+++ src/main/java/com/example/demo/resources/ClienteResource.java
@@ -0,0 +1,35 @@
+package com.example.demo.resources;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categorias;
+import com.example.demo.services.CategoriaService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+
+@RestController
+@RequestMapping(value="/categorias")
+public class ClienteResource {
+
+	
+	@Autowired
+	private CategoriaService service;
+	
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
+		
+		Categorias obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
