892efdfbc736b44fdd0ba79cba6139c2334be651
FrMurta <fernandormurta@gmail.com>
2018-09-05 13:31:39 -0300

    DTO Cat

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.example.demo.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.domain.Categorias;
+import com.example.demo.dto.CategoriaDTO;
 import com.example.demo.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -57,5 +60,13 @@ public class CategoriaResource {
 		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> selectAll() throws ObjectNotFoundException {
+		
+		List<Categorias> list = service.selectAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);	
 	}
 }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +51,11 @@ public class CategoriaService {
 			throw new DataIntegrityViolationException("Não é possivle excluir uma categoria que possui produtos cadastrados");
 		}
 		
+	}
+	
+	public List<Categorias> selectAll(){
+		
+		return repo.findAll();
 	}
 }
 
